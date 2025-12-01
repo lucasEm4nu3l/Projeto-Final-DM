8db28cdc92c11fd3dfe7f831c772efb40c6da4d1
lucasEm4nu3l <lucasb.silva130@gmail.com>
2025-12-01 10:36:14 -0300

    modificando o index

--- p5a-financas2025.2/app/routes/index.js
+++ p5a-financas2025.2/app/routes/index.js
@@ -4,7 +4,6 @@ import { View } from 'react-native';
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
 import { AuthContext } from '../context/auth';
-import Header from '../components/Header/Header';
 
 function Routes(){
   const { signed } = useContext(AuthContext);
